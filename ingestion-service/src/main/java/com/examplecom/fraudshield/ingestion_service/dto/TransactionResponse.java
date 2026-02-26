887b9df5eae901c0b336f4506debbebc05c52706
RayanMalki <rayanmalki54@gmail.com>
2026-02-26 01:39:04 -0500

    Update TransactionResponse.java

--- ingestion-service/src/main/java/com/examplecom/fraudshield/ingestion_service/dto/TransactionResponse.java
+++ ingestion-service/src/main/java/com/examplecom/fraudshield/ingestion_service/dto/TransactionResponse.java
@@ -1,4 +1,17 @@
 package com.examplecom.fraudshield.ingestion_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransactionResponse {
+    String transactionId;
+    boolean fraudulent;
+    double confidenceScore;
+    String status;
+
+
 }
