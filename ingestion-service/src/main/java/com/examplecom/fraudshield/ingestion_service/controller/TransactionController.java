2024c32d65fb6fa80700fa78941a1c9479d4e453
RayanMalki <rayanmalki54@gmail.com>
2026-02-26 01:38:56 -0500

    Update TransactionController.java

--- ingestion-service/src/main/java/com/examplecom/fraudshield/ingestion_service/controller/TransactionController.java
+++ ingestion-service/src/main/java/com/examplecom/fraudshield/ingestion_service/controller/TransactionController.java
@@ -1,4 +1,27 @@
 package com.examplecom.fraudshield.ingestion_service.controller;
 
+import com.examplecom.fraudshield.ingestion_service.dto.TransactionRequest;
+import com.examplecom.fraudshield.ingestion_service.dto.TransactionResponse;
+import com.examplecom.fraudshield.ingestion_service.service.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/transactions")
+@RequiredArgsConstructor
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @PostMapping("/analyze")
+    public ResponseEntity<TransactionResponse> analyzeTransaction
+            (@RequestBody TransactionRequest transactionRequest) {
+
+        return ResponseEntity.ok(transactionService.analyze(transactionRequest));
+
+    }
 }
