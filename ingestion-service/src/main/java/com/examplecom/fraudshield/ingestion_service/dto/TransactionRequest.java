0ec0bfb81ac763e3151ce62fda9568236d4ff1a4
RayanMalki <rayanmalki54@gmail.com>
2026-02-26 01:38:59 -0500

    Update TransactionRequest.java

--- ingestion-service/src/main/java/com/examplecom/fraudshield/ingestion_service/dto/TransactionRequest.java
+++ ingestion-service/src/main/java/com/examplecom/fraudshield/ingestion_service/dto/TransactionRequest.java
@@ -1,4 +1,22 @@
 package com.examplecom.fraudshield.ingestion_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
 public class TransactionRequest {
+
+    String transactionId;
+    String cardNumber;
+    Double amount;
+    String merchant;
+    String location;
+    LocalDateTime timestamp;
 }
