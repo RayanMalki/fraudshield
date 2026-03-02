baea2e5745c59ea455781fcbd794fa6010937ced
RayanMalki <rayanmalki54@gmail.com>
2026-03-02 16:42:44 -0500

    Update PredictionResult.java

--- ingestion-service/src/main/java/com/examplecom/fraudshield/ingestion_service/dto/PredictionResult.java
+++ ingestion-service/src/main/java/com/examplecom/fraudshield/ingestion_service/dto/PredictionResult.java
@@ -11,7 +11,7 @@ import lombok.AllArgsConstructor;
 public class PredictionResult {
 
     @JsonProperty("is_fraud")
-    private boolean is_fraud;
+    private boolean fraud;
 
     private double confidence;
 }
