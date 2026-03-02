7a3e4b5acba92fcb3392db7f06be5bd09384c871
RayanMalki <rayanmalki54@gmail.com>
2026-03-02 15:15:06 -0500

    Create PredictionResult.java

---
+++ ingestion-service/src/main/java/com/examplecom/fraudshield/ingestion_service/dto/PredictionResult.java
@@ -0,0 +1,17 @@
+package com.examplecom.fraudshield.ingestion_service.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PredictionResult {
+
+    @JsonProperty("is_fraud")
+    private boolean is_fraud;
+
+    private double confidence;
+}
